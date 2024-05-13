470d3cb8f1d74696d3d50eba01aa73674f583502
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-13 11:45:48 +0200

    added script

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -1,2 +1,15 @@
 // L'utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore.
+
+const numeroA = prompt("Ciao, inserisci per favore il primo numero");
+const numeroB = prompt("Perfetto, ora scrivi qui il secondo");
+
+let numeroMaggiore;
+
+if (numeroA > numeroB) {
+    numeroMaggiore = numeroA;
+} else {
+    numeroMaggiore = numeroB
+}
+
+console.log(numeroMaggiore)
