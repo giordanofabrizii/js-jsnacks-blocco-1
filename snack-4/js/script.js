3059978e24a39dc0c9a7cbc7ecd932d08455a398
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-13 12:11:45 +0200

    added new function to manage the guests

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -1,11 +1,21 @@
 //  In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
 
 const listaInvitati = ["Riccardo","Mario","Gina","Paolo","Giulia","Steve Jobs"];
+let blackList = [];
 
 let nomeUtente = prompt("Ciao, scrivi qui il tuo nome per scoprire se sei stato invitato");
 
 if (listaInvitati.includes(nomeUtente)) {
     console.log("Sei stato invitao, puoi partecipare");
+    
+    // rimuovo il suo nome
+    let index = listaInvitati.indexOf(nomeUtente);
+    listaInvitati.splice(index, 1);
+
 } else {
     console.log("Mi dispiace, non puoi partecipare");
+
+    // lo aggiungo alla lista nera
+    blackList.push(nomeUtente)
+    console.log(blackList);
 }
