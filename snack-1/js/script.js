c44b856c09be79d596efe282fb076eecffe6bb75
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-13 11:52:14 +0200

    added parseInt in the prompt

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,8 +1,8 @@
 // L'utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore.
 
-const numeroA = prompt("Ciao, inserisci per favore il primo numero");
-const numeroB = prompt("Perfetto, ora scrivi qui il secondo");
+const numeroA = Number.parseInt(prompt("Ciao, inserisci per favore il primo numero"));
+const numeroB = Number.parseInt(prompt("Perfetto, ora scrivi qui il secondo"));
 
 let numeroMaggiore;
 
