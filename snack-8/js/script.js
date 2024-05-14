fea232a324a17ae753e9e2ab03f887dd299b4e9f
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-14 11:36:31 +0200

    created div in the HTML by javascript

--- snack-8/js/script.js
+++ snack-8/js/script.js
@@ -1,1 +1,11 @@
 //  Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l'altro di verde. Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari.
+
+const containerEl = document.getElementById("container");
+
+let divRosso = document.createElement("div")
+divRosso.classList = "bg-rosso"
+containerEl.appendChild(divRosso)
+
+let divVerde = document.createElement("div")
+divVerde.classList = "bg-verde"
+containerEl.appendChild(divVerde)
